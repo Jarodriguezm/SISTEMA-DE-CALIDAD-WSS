ac5b25bc4ab209303d9ca7590949a2a3943f47f1
Jarodriguezm <96915493+Jarodriguezm@users.noreply.github.com>
2026-07-23 17:35:24 -0400

    fix: ESM export default

--- api/asistente-texto.js
+++ api/asistente-texto.js
@@ -13,7 +13,7 @@ const NOMBRES_SECCION = {
   recomendaciones:    'Recomendaciones',
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
